ce1de9a5baa248082936a6dbf268ae416c6b37f0
nataliafalla <natalia.falla@utec.edu.pe>
2023-10-26 12:04:18 -0500

    frontend

---
+++ proyecto-final-parcial-6/frontend/scrit.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginButton").addEventListener('click', function () {
+        // Redirecciona al usuario a la página de inicio de sesión
+        //window.location.replace = 'login.html';
+        alert('Register button clicked');
+
+    });
+
+    document.getElementById('registerButton').addEventListener('click', function () {
+        // Aquí puedes mantener la funcionalidad original o redireccionar a una página de registro
+        alert('Register button clicked');
+    });
+});
+
+
